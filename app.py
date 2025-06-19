fd6d5065b2cad29f739c7d18cc4af833b12263e8
Arnel <167875243+Arnel-rah@users.noreply.github.com>
2025-06-19 12:39:02 +0300

    chore: add certifacte SSL

--- app.py
+++ app.py
@@ -277,5 +277,12 @@ def initdb_command():
     db.create_all()
     print('Base de données initialisée.')
 
+# ... Votre code existant ...
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(
+        debug=True,
+        ssl_context=('server.crt', 'server.key'),
+        host='0.0.0.0',
+        port=443
+    )
